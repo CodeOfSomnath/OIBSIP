fd61fec2ae437d6943405d18ac866e82fc454b24
CodeOfSomnath <somnathnandi368@gmail.com>
2024-10-10 22:19:48 +0530

    limit of rounds

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -3,34 +3,60 @@ import java.util.Scanner;
 
 public class GuessTheNumber {
 
-
-    public static void main(String[] args) {
+    public static void mainloop(int numberOfGuess, Scanner sc) {
         int maxNumber = 100, minNumber = 1;
+        int countOfGuess = 0;
+
         Random random = new Random();
-        // random.setSeed(System.currentTimeMillis());
         int randomNumber = random.nextInt(minNumber, maxNumber);
-
-        System.out.println("***** Welcome to Guessing Game ******");
-        System.out.println("You have to guess the computer generated number. Good Luck!");
-
-        Scanner sc = new Scanner(System.in);
+        
         int guessNumber = 0;
         while (randomNumber != guessNumber) {
-            System.out.println("Enter your guess: ");
+            
+            // limiting number of attempts
+            if (countOfGuess == numberOfGuess ) {
+                System.out.println("You ran out of attempts.");
+                System.out.println("You have lost the game.");
+                System.out.printf("The correct number was %d\n", randomNumber);
+                break;
+            }
+
+            System.out.printf("Enter your %dth guess: ", countOfGuess);
             guessNumber = sc.nextInt();
             if (randomNumber > guessNumber) {
                 System.out.println("number is too low.");
             } else if (randomNumber < guessNumber) {
                 System.out.println("Number is too high.");
             } else {
-                System.out.println("You have guessed the number.");
+                System.out.printf("You have guessed the number in %d/%d attempts\n", countOfGuess, numberOfGuess);
                 System.out.println("Congrats! you have won.");
             }
+            countOfGuess++;
         }
 
+    }
 
+    public static void main(String[] args) {
+
+        int numberOfGuess = 10;
+
+        System.out.println("***** Welcome to Guessing Game ******");
+        System.out.println("You have to guess the computer generated number. Good Luck!");
+
+        Scanner sc = new Scanner(System.in);
+        String op;
+        mainloop(numberOfGuess, sc);
+        sc.nextLine();
+        while (true) {
+            System.out.println("Do you want to play more(y/n)? ");
+            op = sc.nextLine();
+            if (op.strip().toLowerCase().equals("y")) {
+                mainloop(numberOfGuess, sc);
+            } else if (op.strip().toLowerCase().equals("n")) {
+                System.out.println("Thank you for playing our game.");
+                break;
+            }
+        }
         sc.close();
-
-
     }
 }
